bb4d916dcc02ffaaa0afba3c7a35ceb97954fe8e
Dbenjaminmeza <benjamin.meza30@gmail.com>
2025-05-16 10:23:03 -0300

    se agrega mejoras como los indices a la tabla

--- 3_bucle_for_tabla_multiplicar.py
+++ 3_bucle_for_tabla_multiplicar.py
@@ -1,7 +1,8 @@
-numero = int(input("ingresar un numero: "))
+print("---Tabla de multiplicar---")
 
+numero = int(input("ingresar un numero: "))
 
 for i in range(1,11):
     calculo = numero * i
-    print(calculo)
+    print(f"{numero} x {i} = {calculo}")
 
