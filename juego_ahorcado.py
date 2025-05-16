5b449fe3d6c95740de57cb8058d255e1e0b71cc3
Dbenjaminmeza <benjamin.meza30@gmail.com>
2025-05-16 10:52:49 -0300

    se agrega manejo de errores pasa a estado alpha el proyecto

--- juego_ahorcado.py
+++ juego_ahorcado.py
@@ -1,32 +1,36 @@
+# EL JUEGO SE ENCUENTRA EN FASE ALPHA
 palabras = ["metacrilato","otorrinolaringologia","paralelepipedo"]
 intentos = 0
 bandera = False
+numero_correcto = False
 
 print("Hola, bienvenido al ahorcado, en este juego vas a tener 10 intentos para adivinar la palabra oculta tene en cuenta que por cada intento fallido se resta uno. Mucha suerte!")
 
 numero_usuario = input("primero: te pido que ingreses un numero del 1 al 3 para poder empezar ")
 
-try:
-    numero = int(numero_usuario)
-except ValueError: 
-    input("el numero no es valido, ingrese otro numero")
-    numero = 1
-    
+while numero_correcto == False:
+    try:
+        numero = int(numero_usuario)
+        if numero == 1:
+            palabra_elegida = palabras[0]
+            numero_correcto = True
+        elif numero == 2:
+            palabra_elegida = palabras[1]
+            numero_correcto = True
+        elif numero == 3:
+            palabra_elegida = palabras[2]
+            numero_correcto = True
+        else:
+            numero_usuario = input("el numero no es valido, ingrese un numero del 1 al 3 ")
+    except ValueError: 
+       numero_usuario = input("el dato no es valido, ingrese un numero del 1 al 3: ")
 
-
-if numero == 1:
-    palabra_elegida = palabras[0]
-elif numero == 2:
-    palabra_elegida = palabras[1]
-elif numero == 3:
-    palabra_elegida = palabras[2]
-    
 
 print("tu palabra elegida es la siguiente")
 
 palabra_oculta = len(palabra_elegida) * "_"
 print(palabra_oculta)
-
+# EL CODIGO REPRODUCE CORRECTAMENTE HASTA LA LINEA 32 EL RESTO IRE MEJORANDO
 while intentos < 10 and bandera == False:
     letra = input ("ingresa una letra: ")
     letra_minuscula = letra.lower()
