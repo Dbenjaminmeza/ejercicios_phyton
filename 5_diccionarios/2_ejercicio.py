333339202f76ca470e888bf42a0e962346bbafcc
Dbenjaminmeza <benjamin.meza30@gmail.com>
2025-05-26 20:36:06 -0300

    se organiza carpeta completa

---
+++ 5_diccionarios/2_ejercicio.py
@@ -0,0 +1,10 @@
+""" 1-Crear un diccionario con los nombres de tres frutas y sus respectivos
+precios y mostrar el diccionario completo. """
+
+oferta = {
+    "manzana": 2500,
+    "banana": 2000,
+    "ciruela": 1500
+}
+
+print(oferta)
